d0257445c76b3f722b79e0ed497602b9fc6a034e
Quentin Oschatz <quentin@cliffhouse.de>
2018-05-15 17:22:39 +0200

    feat$(Vue, login): Add Login and Logout frontend for Github

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -12,7 +12,21 @@
 
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn to="/dashboard" flat>Dashboard</v-btn>
-        <v-btn flat>Login</v-btn>
+        <v-btn flat href="http://localhost:5000/api/github-logout" v-if="loggedIn">Logout</v-btn>
+        <v-btn flat @click="loginDialog=true" v-if="!loggedIn">Login</v-btn>
+        <v-dialog v-model="loginDialog" max-width="500px">
+        <v-card>
+          <v-card-title>
+            Login
+          </v-card-title>
+          <v-card-text>
+            <v-btn color="grey darken-4" style="color: white" href="http://localhost:5000/api/github-login"><span style="font-size: 2em; margin-right: 10px;"><i class="fab fa-github"></i></span> Login with Github</v-btn>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" flat @click.stop="loginDialog=false">Close</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
       </v-toolbar-items>
 
     </v-toolbar>
@@ -21,11 +35,31 @@
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   name: 'navbar',
   components: {
 
+  },
+  data() {
+    return {
+      loginDialog: false,
+      github: "",
+      loggedIn: false,
+      user: {}
+    }
+  },
+  // See if a user is logged in
+  created: function () {
+    axios.get("http://localhost:5000/api/github-user")
+    .then(resp => {
+      if(!(resp.data = "401: Bad credentials")) {
+        this.loggedIn = true
+        this.user = resp.data
+        console.log(resp)
+      }
+    })
   }
 }
 </script>

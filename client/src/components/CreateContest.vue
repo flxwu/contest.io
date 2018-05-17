bfb0fa5f6efeea6ba34881284e6a275d517d5eda
Quentin Oschatz <quentin@cliffhouse.de>
2018-05-17 11:34:36 +0200

    feat$(Vue, contestcreate): Add detail popup

--- client/src/components/CreateContest.vue
+++ client/src/components/CreateContest.vue
@@ -31,6 +31,37 @@
 
                 <template v-for="item in filteredItems">
 
+                  <!-- Detail view popup -->
+                  <v-dialog v-model="detailPopup" max-width="500px">
+                    <v-card>
+                      <v-card-title>
+                        Detail
+                      </v-card-title>
+                      <v-card-text>
+                        {{ item.title }}<br>
+                        <small style="margin-right: 10px; float: left; margin-top: 8px; margin-left: 40%;">Difficulty ({{ item.codeforces_index }}): </small>
+                        <v-progress-linear v-if="item.codeforces_index.split('')[0] === 'A'" style="width: 50px; margin-right: 50px; float: left;" value="20" buffer-value="20" color="green"></v-progress-linear>
+                        <v-progress-linear v-else-if="item.codeforces_index.split('')[0] === 'B'" style="width: 50px; margin-right: 50px; float: left;" value="40" buffer-value="40" color="cyan"></v-progress-linear>
+                        <v-progress-linear v-else-if="item.codeforces_index.split('')[0] === 'C'" style="width: 50px; margin-right: 50px; float: left;" value="60" buffer-value="60" color="yellow"></v-progress-linear>
+                        <v-progress-linear v-else-if="item.codeforces_index.split('')[0] === 'D'" style="width: 50px; margin-right: 50px; float: left;" value="80" buffer-value="80" color="orange"></v-progress-linear>
+                        <v-progress-linear v-else-if="item.codeforces_index.split('')[0] === 'E'" style="width: 50px; margin-right: 50px; float: left;" value="100" color="red"></v-progress-linear>
+                        <v-progress-linear v-else style="width: 50px; margin-right: 50px; float: left;" value="100" color="red"></v-progress-linear><br>
+
+                      </v-card-text>
+
+                      <!-- Tags -->
+                      <div class="text-xs-center chiptag">
+                        <v-chip :key="tag" small v-for="tag in item.tags" >{{ tag }}</v-chip>
+                      </div>
+
+                      <v-card-actions>
+
+                        <v-btn flat color="orange" to="https://www.codeforces.org">Solve</v-btn>
+                        <v-btn color="primary" flat @click.stop="detailPopup=false">Close</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+
                    <v-divider :key="item.id + '-divider'"></v-divider>
 
                    <v-list-tile avatar :key="item.id + '-avatar'" @click="alert()">
@@ -39,9 +70,11 @@
                          <v-icon @click="addTask(item.id)">add</v-icon>
                      </v-list-tile-action>
 
-                    <v-list-tile-content>
+                    <v-list-tile-content @click="detailPopup=true">
                        <v-list-tile-title v-html="item.title"></v-list-tile-title>
                     </v-list-tile-content>
+
+
 
                     <small style="margin-right: 10px;">Difficulty ({{ item.codeforces_index }}): </small>
                     <v-progress-linear v-if="item.codeforces_index.split('')[0] === 'A'" style="width: 50px; margin-right: 50px;" value="20" buffer-value="20" color="green"></v-progress-linear>
@@ -51,7 +84,7 @@
                     <v-progress-linear v-else-if="item.codeforces_index.split('')[0] === 'E'" style="width: 50px; margin-right: 50px;" value="100" color="red"></v-progress-linear>
                     <v-progress-linear v-else style="width: 50px; margin-right: 50px;" value="100" color="red"></v-progress-linear>
 
-                    <v-chip :key="tag" small v-for="tag in item.tags.slice(0, 4)" >{{ tag }}</v-chip>
+                    <v-chip :key="tag" small v-for="tag in item.tags.slice(0, 3)" >{{ tag }}</v-chip>
 
                    </v-list-tile>
 
@@ -68,13 +101,15 @@
 
              <v-expansion-panel-content v-for="item in tasks" :key="item.id">
 
-               <div slot="header">{{ item.title }} <br><small style="float: left; margin-top: 9px; margin-right: 10px;">Difficulty ({{ item.codeforces_index }}): </small>
-                   <v-progress-linear v-if="item.codeforces_index.split('')[0] === 'A'" style="float:left; width: 100px;" value="20" buffer-value="20" color="green"></v-progress-linear>
-                   <v-progress-linear v-else-if="item.codeforces_index.split('')[0] === 'B'" style="float:left; width: 100px;" value="40" buffer-value="40" color="cyan"></v-progress-linear>
-                   <v-progress-linear v-else-if="item.codeforces_index.split('')[0] === 'C'" style="float:left; width: 100px;" value="60" buffer-value="60" color="yellow"></v-progress-linear>
-                   <v-progress-linear v-else-if="item.codeforces_index.split('')[0] === 'D'" style="float:left; width: 100px;" value="80" buffer-value="80" color="orange"></v-progress-linear>
-                   <v-progress-linear v-else-if="item.codeforces_index.split('')[0] === 'E'" style="float:left; width: 100px;" value="100" color="red"></v-progress-linear>
-                   <v-progress-linear v-else style="float:left; width: 100px;" value="100" color="red"></v-progress-linear>
+               <div slot="header">
+                  {{ item.title }}<br>
+                  <small style="float: left; margin-top: 9px; margin-right: 10px;">Difficulty ({{ item.codeforces_index }}): </small>
+                   <v-progress-linear v-if="item.codeforces_index.split('')[0] === 'A'" style="float:left; width: 50px;" value="20" buffer-value="20" color="green"></v-progress-linear>
+                   <v-progress-linear v-else-if="item.codeforces_index.split('')[0] === 'B'" style="float:left; width: 50px;" value="40" buffer-value="40" color="cyan"></v-progress-linear>
+                   <v-progress-linear v-else-if="item.codeforces_index.split('')[0] === 'C'" style="float:left; width: 50px;" value="60" buffer-value="60" color="yellow"></v-progress-linear>
+                   <v-progress-linear v-else-if="item.codeforces_index.split('')[0] === 'D'" style="float:left; width: 50px;" value="80" buffer-value="80" color="orange"></v-progress-linear>
+                   <v-progress-linear v-else-if="item.codeforces_index.split('')[0] === 'E'" style="float:left; width: 50px;" value="100" color="red"></v-progress-linear>
+                   <v-progress-linear v-else style="float:left; width: 50px;" value="100" color="red"></v-progress-linear>
                </div>
 
               <v-card>
@@ -83,10 +118,9 @@
 
                 <!-- Tags -->
                 <div class="text-xs-center chiptag">
-                  <v-chip small>Bruteforce</v-chip>
+                  <v-chip :key="tag" small v-for="tag in item.tags" >{{ tag }}</v-chip>
                 </div>
 
-                <!-- TODO: add link to codeforces -->
                 <v-card-actions>
                   <v-btn flat color="red" @click="removeTask(item.id)">Remove</v-btn>
                   <v-btn flat color="orange" to="https://www.codeforces.org">Solve</v-btn>
@@ -170,9 +204,10 @@ export default {
       dialog2: false,
       contestname: "",
       visible: false,
+      detailPopup: false,
       first: 0,
       items: [
-        { id: 1, title: '808 Get your life together', tags: ['Bruteforce', 'Binary Trees'], codeforces_index: "F" },
+        { id: 1, title: '808 Get your life together', tags: ['Bruteforce', 'Binary Trees', 'Binary Trees', 'Binary Trees', 'Binary Trees', 'Binary Trees', '1', '2'], codeforces_index: "F" },
         { id: 2, title: '809 Procrastinate Task 1 until your life is over', tags: ['Bruteforce'], codeforces_index: "A" },
         { id: 3, title: 'Task 3: Drink bleech to get over your depression', tags: ['Bruteforce', 'Binary Trees'], codeforces_index: "A" },
         { id: 4, title: 'Task 4: Live the good life!', tags: ['Bruteforce', 'Binary Trees'], codeforces_index: "A" },

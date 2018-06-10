5d02a5c6d9d77f1eb5731324c27e26546d50fde8
Quentin Oschatz <quentin@cliffhouse.de>
2018-06-10 18:07:27 +0200

    feat$(Vue, dashboard): Basic Setup

--- client/src/components/Dashboard.vue
+++ client/src/components/Dashboard.vue
@@ -3,3 +3,22 @@
     <p>Dashboard</p>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'dashboard',
+  components: {},
+  data() {
+    return {
+    };
+  },
+  mounted() {
+  }
+};
+</script>
+
+<style scoped>
+
+</style>

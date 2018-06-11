99255da5a60b4c3234baea18aa168d29380758fd
Quentin Oschatz <quentin@cliffhouse.de>
2018-06-11 15:12:22 +0200

    feat$(Vue, dashboard): Add User Profile Overview

--- client/src/components/Dashboard.vue
+++ client/src/components/Dashboard.vue
@@ -1,6 +1,34 @@
 <template>
   <div>
-    <p class="">{{ user.login }}</p>
+    <!-- Layout container -->
+    <v-container fluid>
+
+      <v-layout>
+
+        <v-flex xs3>
+          <v-card class="cardprofile">
+
+            <v-card-title primary-title>
+              <div  style="width: 100% !important">
+                <h3 class="headline mb-0">Profile</h3>
+                <v-divider></v-divider><br>
+                <v-avatar size="128px"><img :src="user.avatar_url" alt="avatar"></v-avatar><br><br>
+                <div>Username:  {{ user.login }}  ({{ user.name }})</div>
+                <div>Email:  {{ user.email }} </div><br>
+                <div>{{ user.bio }} </div><br>
+              </div>
+            </v-card-title>
+
+          </v-card>
+        </v-flex>
+
+        <v-flex xs9>
+
+        </v-flex>
+
+      </v-layout>
+
+    </v-container>
   </div>
 </template>
 
@@ -21,7 +49,9 @@ export default {
       .then(resp => {
         if(!(resp.data == '401: Bad credentials')) {
           this.user = resp.data;
-          console.log(typeof(this.user));
+          console.log(this.user);
+        } else {
+          window.location = history.go(-1);
         }
       });
   }
